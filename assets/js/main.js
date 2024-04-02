efd766a0f50acd690f295abf9a724fc7aec96a5a
Yasir Ali <sdr.yasir@gmail.com>
2024-04-02 04:12:12 -0700

    javascript started

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+//functions in JavaScript
+function ourTime(){
+    alert("Monday to Friday | 9:00 - 5:00");
+}
+function ourLocation(){
+    alert("We are located in Saddar RWP, Main Haider Road");
+}
+function ourCleaners(){
+    alert("We have many expert cleaners");
+}
+
